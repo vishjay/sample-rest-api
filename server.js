c1a7d9b8fcf9366dad4c4bd5ab51a804fbe95126
IsuruJ-PC <isuru.jayathilake@itelasoft.com>
2019-04-07 21:10:00 +0530

    category changed to categories

--- server.js
+++ server.js
@@ -9,37 +9,37 @@ const movies = [
         id: "243ff4eb-5ff6-4ed3-a17f-64c42140bd4f",
         name: "Venom",
         imdb: 7.6,
-        category: ["superhero"]
+        categories: ["superhero"]
     },
     {
         id: "5653821e-857a-4e65-826c-aecfe81c01e1",
         name: "Bohemian Rhapsody",
         imdb: 8.1,
-        category: ["music"]
+        categories: ["music"]
     },
     {
         id: "38523f0d-0ec6-48c2-89b6-27382d3abb9d",
         name: "Aquaman",
         imdb: 8.0,
-        category: ["superhero"]
+        categories: ["superhero"]
     },
     {
         id: "568fd428-34cb-46ca-be97-d8aed1a4b649",
         name: "Annihilation",
         imdb: 6.7,
-        category: ["horror"]
+        categories: ["horror"]
     },
     {
         id: "7b2579d1-37bf-49c0-8cab-a45685ad5198",
         name: "Mission: Impossible – Fallout",
         imdb: 8.0,
-        category: ["action"]
+        categories: ["action"]
     },
     {
         id: "1f6f83fb-38a4-4de1-98be-c08ee98b0bbd",
         name: "First Man",
         imdb: 6.4,
-        category: ["history"]
+        categories: ["history"]
     }
 
 ];
@@ -81,7 +81,7 @@ app.post('/api/movies', (req, res) => {
 
     movie.id = uuidv1().toString();
     movie.name = (req.body.name) ? req.body.name : "";
-    movie.category = (req.body.category) ? req.body.category : "";
+    movie.categories = (req.body.categories) ? req.body.categories : "";
     movie.imdb = (req.body.imdb) ? req.body.imdb : "";
 
     movies.push(movie);
